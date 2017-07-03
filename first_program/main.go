2eba6cb0ec6bfa310aa518a99ffd9fd2ebdcd073
Tom Elliott <tomxelliott@hotmail.co.uk>
2017-07-03 23:20:43 +0100

    Create main.go

---
+++ first_program/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var x int = 5
+	var statement string = "This is cooooool!"
+	z:= "hello"
+	fmt.Println("I dig GoLang")
+	fmt.Println(x)
+	fmt.Println(statement)
+	fmt.Println(z)
+	if x == 5 {
+		fmt.Println("5 Alive Baby!!!")
+	}
+}
+
+// The := syntax is shorthand for declaring and initializing a variable 
+// e.g. for var f string = "short" in this case.
